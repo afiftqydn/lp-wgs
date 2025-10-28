7864ec529061cb299f1bcc64339d8d309bb4099a
afiftqydn <afiftqydn00@gmail.com>
2025-10-28 16:36:51 +0700

    Add News & Gallery

--- src/data/page5Data.js
+++ src/data/page5Data.js
@@ -1,97 +1,177 @@
-// src/data/page5Data.js
-
-// Catatan: newsArticles diisi dengan data dari Berkat News TV (image_3025b8.jpg)
 export const newsArticles = [
-    { 
-      id: 10, // Urutan terbaru di atas
-      title: "Andalus dan WGS Kerjasama Pembiayaan Umroh", 
-      date: "14 Jul 2025", 
-      summary: "Berkatnewstv, Travel Andalusia menjalin Kerjasama dengan PT Win Global Solusitama (WGS) untuk pembiayaan umroh...",
-      imgSrc: "/andalus.jpg",
-      sourceUrl: "https://berkatnewstv.com/andalus-dan-wgs-kerjasama-pembiayaan-umroh/" // URL sumber
+    { 
+      id: 20, // Urutan terbaru di atas
+      title: "Holding Firmans Grup Hadir di Kalbar, Berbisnis Dibarengi Sosial Kemanusiaan", 
+      date: "22 Okt 2025", 
+      summary: "Berkatnewstv, Holding Firmans Grup kini hadir di Kalimantan Barat. CEO PT Win Global Solusitama (WGS) Hendra Firmansyah...",
+      imgSrc: "/firmansgrup.jpg",
+      sourceUrl: "https://berkatnewstv.com/2025/10/22/holding-firmans-grup-hadir-di-kalbar-berbisnis-dibarengi-sosial-kemanusiaan/" // URL sumber
+    },
+    { 
+      id: 19, 
+      title: "Gagas Bank Wakaf Pertama di Dunia, Kemenag Kalbar Apresiasi Win Global Solution", 
+      date: "22 Okt 2025", 
+      summary: "Suara Pemred Kalbar, Kemenag Kalbar mengapresiasi CEO Win Global Solution (WGS), Hendra Firmansyah, yang menggagas Bank Wakaf pertama...",
+      imgSrc: "/bankwakaf_pemred.jpg",
+      sourceUrl: "https://www.suarapemredkalbar.com/read/potret/22102025/gagas-bank-wakaf-pertama-di-dunia-kemenag-kalbar-apresiasi-win-global-solution" // URL sumber
+    },
+  { 
+      id: 18, // Urutan terbaru di atas
+      title: "Food Media, Aplikasi Karya Anak Bangsa Hadirkan Harga Makanan Online Setara Offline", 
+      date: "21 Okt 2025", 
+      summary: "CyberTV.id, Food Media, aplikasi karya anak bangsa, resmi diluncurkan di WGS Building, Pontianak. Aplikasi ini menawarkan harga makanan online...",
+      imgSrc: "/foodmedia_cybertv.jpg",
+      sourceUrl: "https://cybertv.id/food-media-aplikasi-karya-anak-bangsa-hadirkan-harga-makanan-online-setara-offline-dan-dukung-umkm-kalbar/" // URL sumber
+    },
+    { 
+      id: 17, 
+      title: "Lawan Dominasi Asing, Anak Bangsa Luncurkan Food Media: Murah, Aman dan Berdayakan UMKM", 
+      date: "21 Okt 2025", 
+      summary: "Fakta Investigasi News, Dalam upaya melawan dominasi asing, anak bangsa meluncurkan aplikasi Food Media di Pontianak...",
+      imgSrc: "/foodmedia_fin.jpg",
+      sourceUrl: "https://faktainvestigasinews8.wordpress.com/2025/10/21/lawan-dominasi-asing-anak-bangsa-luncurkan-food-media-murah-aman-dan-berdayakan-umkm-lokal/" // URL sumber
+    },
+    { 
+      id: 16, 
+      title: "Dari Anak Bangsa Untuk Bangsa, FoodMedia Hadir di Kota Pontianak", 
+      date: "21 Okt 2025", 
+      summary: "Media Harian Nusantara, Aplikasi FoodMedia resmi hadir di Kota Pontianak, diluncurkan di WGS Building. Ini merupakan karya anak bangsa...",
+      imgSrc: "/foodmedia_mhn.jpg",
+      sourceUrl: "https://mediahariannusantara.wordpress.com/2025/10/21/dari-anak-bangsa-untuk-bangsa-foodmedia-hadir-di-kota-pontianak/" // URL sumber
+    },
+    { 
+      id: 15, 
+      title: "Win Global Solusitama Building dan Aplikasi FoodMedia Resmi Diluncurkan di Pontianak", 
+      date: "21 Okt 2025", // Tanggal peluncuran
+      summary: "Pontianak Info, PT Win Global Solusitama (WGS) meresmikan WGS Building sekaligus meluncurkan aplikasi FoodMedia, sebuah platform kuliner...",
+      imgSrc: "/foodmedia.jpg",
+      sourceUrl: "https://pontianakinfo.disway.id/amp/18773/win-global-solusitama-building-dan-aplikasi-foodmedia-resmi-diluncurkan-di-pontianak" // URL sumber
+    },
+    { 
+      id: 14, 
+      title: "Kemenag Kalbar Apresiasi WGS Gagas Bank Wakaf", 
+      date: "22 Okt 2025", 
+      summary: "Pontianak, RRI, Ketua Tim Humas, Data dan Informasi, Aspari apresiasi CEO Win Global Solution (WGS), Hendra Firmansyah yang menggagas Bank Wakaf...",
+      imgSrc: "/bankwakaf.jpg",
+      sourceUrl: "https://rri.co.id/daerah/1919295/kemenag-kalbar-apresiasi-wgs-gagas-bank-wakaf" // URL sumber
+    },
+    { 
+      id: 13, 
+      title: "Borneo Fair Reborn 2025, Sajikan Hiburan dan Dorong Ekonomi Lokal", 
+      date: "28 Sep 2025", 
+      summary: "HARIAN DISWAY, PT Win Global Solusitama (WGS). Ia menegaskan, tujuan utama dari penyelenggaraan Borneo Fair Reborn 2025 adalah menghadirkan...",
+      imgSrc: "/borneofair.jpg",
+      sourceUrl: "https://harian.disway.id/read/901032/borneo-fair-reborn-2025-sajikan-hiburan-dan-dorong-ekonomi-lokal" // URL sumber
+    },
+    { 
+      id: 12, 
+      title: "Borneo Fair 2025 di Pontianak Bakal Dimeriahkan Rony Parulian", 
+      date: "15 Sep 2025", 
+      summary: "Liputan6.com, Borneo Fair 2025 diselenggarakan WGS untuk mendukung pertumbuhan UMKM lokal. Musisi papan atas seperti Rony Parulian akan tampil...",
+      imgSrc: "/ronyparulian.jpg",
+      sourceUrl: "https://www.liputan6.com/showbiz/read/6160092/borneo-fair-2025-di-pontianak-bakal-dimeriahkan-rony-parulian-ceo-wgs-kasih-bocoran-begini" // URL sumber
+    },
+    { 
+      id: 11, 
+      title: "Staff Ahli Bupati Ketapang Terima Audiensi Bersama PT. Win Global Solusitama (WGS)", 
+      date: "4 Agu 2025", 
+      summary: "Ketapang, SETDA, Staff Ahli Bupati Maryadi Asmu'ie menyambut baik penjelasan pihak PT. WGS dan akan menyampaikan hasil audiensi...",
+      imgSrc: "/audiketapang.jpg",
+      sourceUrl: "https://setda.ketapangkab.go.id/berita/2025-08-04-menerima-audiensi-bersama-pt-win-global-solusitama-wgs-staff-ahli-bupati-menyambut-niat-baik-dan-sekaligus-silahturami-ke-pemerintah-kabupaten-ketapang-bertempat-di-ruang-rapat-sekda" 
     },
-    { 
-      id: 9, 
-      title: "Pemkab Kubu Raya – WGS Kerjasama Payroll TPP ASN", 
-      date: "9 Jul 2025", 
-      summary: "Kubu Raya, Berkatnewstv, Pemerintah Kabupaten Kubu Raya secara resmi telah membangun kerjasama dengan PT Win...",
-      imgSrc: "/payroll.jpg",
-      sourceUrl: "https://berkatnewstv.com/pemkab-kubu-raya-wgs-kerjasama-payroll-tpp-asn/" // URL sumber
-    },
-    { 
-      id: 8, 
-      title: "Kemenag Kalbar dan WGS Kerja Sama Pembiayaan Syariah", 
-      date: "4 Jun 2025", 
-      summary: "Pontianak, Berkatnewstv, Kantor Wilayah Kementerian Agama Kalbar akan bekerja sama dengan PT Win Global Solusitama...",
-      imgSrc: "/kemenag.jpg",
-      sourceUrl: "https://berkatnewstv.com/kemenag-kalbar-dan-wgs-kerja-sama-pembiayaan-syariah/" // URL sumber
-    },
-    { 
-      id: 7, 
-      title: "Kolaborasi WGS Entaskan Stunting di Harganas Kalbar", 
-      date: "27 Mei 2025", 
-      summary: "Pontianak, Berkatnewstv, Di tengah hiruk pikuk kehidupan, acap kali kita mendengar kabar tentang derita...",
-      imgSrc: "/stunting.jpg",
-      sourceUrl: "https://berkatnewstv.com/kolaborasi-wgs-entaskan-stunting-di-harganas-kalbar/" // URL sumber
-    },
-    { 
-      id: 6, 
-      title: "Kerjasama dengan Korpri, WGS Biayai Umroh 9,7 Juta ASN", 
-      date: "22 Mei 2025", 
-      summary: "Breaking News, Kubu Raya, PT Win Global Solusitama (WGS) telah melakukan Perjanjian Kerjasama (PKS) dengan DPK...",
-      imgSrc: "/korpri.jpg",
-      sourceUrl: "https://berkatnewstv.com/kerjasama-dengan-korpri-wgs-biayai-umroh-9-7-juta-asn/" // URL sumber
-    },
-    { 
-      id: 5, 
-      title: "UMKM Singkawang Lega Mendapat Pembinaan WGS", 
-      date: "14 Mei 2025", 
-      summary: "Singkawang, Berkatnewstv, PT Win Global Solusitama (WGS) semakin merambah para UMKM. Kali ini yang menjadi...",
-      imgSrc: "/umkmsingkawang.jpg",
-      sourceUrl: "https://berkatnewstv.com/umkm-singkawang-lega-mendapat-pembinaan-wgs/" // URL sumber
-    },
-    { 
-      id: 4, 
-      title: "WGS Solusi Atasi Warga Desa dari Jeratan Rentenir", 
-      date: "5 Mei 2025", 
-      summary: "Kubu Raya, Berkatnewstv, Kehadiran PT Win Global Solusitama (WGS) semakin hari kian mendapat tempat di...",
-      imgSrc: "/rentenir.jpg",
-      sourceUrl: "https://berkatnewstv.com/wgs-solusi-atasi-warga-desa-dari-jeratan-rentenir/" // URL sumber
-    },
-    { 
-      id: 3, 
-      title: "WGS Melayani Pembiayaan untuk Semua Umat", 
-      date: "30 April 2025", 
-      summary: "Pontianak, Berkatnewstv, PT Win Global Solusitama (WGS), sebuah perusahaan star-up berbasis syariah yang serius...",
-      imgSrc: "/semuaumat.jpg",
-      sourceUrl: "https://berkatnewstv.com/wgs-melayani-pembiayaan-untuk-semua-umat/" // URL sumber
-    },
-    { 
-      id: 2, 
-      title: "WGS Angin Segar UMKM di Tengah Ekonomi Tidak Stabil", 
-      date: "29 April 2025", 
-      summary: "Pontianak, Berkatnewstv, Wakil Ketua Pontianak City Rudi Kamtono mengapresiasi positif kehadiran PT Win Global Solusitama...",
-      imgSrc: "/angin.jpg",
-      sourceUrl: "https://berkatnewstv.com/wgs-angin-segar-umkm-di-tengah-ekonomi-tidak-stabil/" // URL sumber
-    },
-    { 
-      id: 1, 
-      title: "Pemprov Kalbar Dukung WGS Bantu Pembiayaan UMKM", 
-      date: "29 April 2025", 
-      summary: "Pontianak, Berkatnewstv, Pemprov Kalbar menyatakan dukungannya terhadap PT Win Global Solusitama (WGS) membantu pembiayaan UMKM...",
-      imgSrc: "/pemprov.jpg",
-      sourceUrl: "https://berkatnewstv.com/pemprov-kalbar-dukung-wgs-bantu-pembiayaan-umkm/" // URL sumber
-    },
+    { 
+      id: 10, 
+      title: "Andalus dan WGS Kerjasama Pembiayaan Umroh", 
+      date: "14 Jul 2025", 
+      summary: "Berkatnewstv, Travel Andalusia menjalin Kerjasama dengan PT Win Global Solusitama (WGS) untuk pembiayaan umroh...",
+      imgSrc: "/andalus.jpg",
+      sourceUrl: "https://berkatnewstv.com/andalus-dan-wgs-kerjasama-pembiayaan-umroh/" // URL sumber
+    },
+    { 
+      id: 9, 
+      title: "Pemkab Kubu Raya – WGS Kerjasama Payroll TPP ASN", 
+      date: "9 Jul 2025", 
+      summary: "Kubu Raya, Berkatnewstv, Pemerintah Kabupaten Kubu Raya secara resmi telah membangun kerjasama dengan PT Win...",
+      imgSrc: "/payroll.jpg",
+      sourceUrl: "https://berkatnewstv.com/pemkab-kubu-raya-wgs-kerjasama-payroll-tpp-asn/" // URL sumber
+    },
+    { 
+      id: 8, 
+      title: "Kemenag Kalbar dan WGS Kerja Sama Pembiayaan Syariah", 
+      date: "4 Jun 2025", 
+      summary: "Pontianak, Berkatnewstv, Kantor Wilayah Kementerian Agama Kalbar akan bekerja sama dengan PT Win Global Solusitama...",
+      imgSrc: "/kemenag.jpg",
+      sourceUrl: "https://berkatnewstv.com/kemenag-kalbar-dan-wgs-kerja-sama-pembiayaan-syariah/" // URL sumber
+    },
+    { 
+      id: 7, 
+      title: "Kolaborasi WGS Entaskan Stunting di Harganas Kalbar", 
+      date: "27 Mei 2025", 
+      summary: "Pontianak, Berkatnewstv, Di tengah hiruk pikuk kehidupan, acap kali kita mendengar kabar tentang derita...",
+      imgSrc: "/stunting.jpg",
+      sourceUrl: "https://berkatnewstv.com/kolaborasi-wgs-entaskan-stunting-di-harganas-kalbar/" // URL sumber
+    },
+    { 
+      id: 6, 
+      title: "Kerjasama dengan Korpri, WGS Biayai Umroh 9,7 Juta ASN", 
+      date: "22 Mei 2025", 
+      summary: "Breaking News, Kubu Raya, PT Win Global Solusitama (WGS) telah melakukan Perjanjian Kerjasama (PKS) dengan DPK...",
+      imgSrc: "/korpri.jpg",
+      sourceUrl: "https://berkatnewstv.com/kerjasama-dengan-korpri-wgs-biayai-umroh-9-7-juta-asn/" // URL sumber
+    },
+    { 
+      id: 5, 
+      title: "UMKM Singkawang Lega Mendapat Pembinaan WGS", 
+      date: "14 Mei 2025", 
+      summary: "Singkawang, Berkatnewstv, PT Win Global Solusitama (WGS) semakin merambah para UMKM. Kali ini yang menjadi...",
+      imgSrc: "/umkmsingkawang.jpg",
+      sourceUrl: "https://berkatnewstv.com/umkm-singkawang-lega-mendapat-pembinaan-wgs/" // URL sumber
+    },
+    { 
+      id: 4, 
+      title: "WGS Solusi Atasi Warga Desa dari Jeratan Rentenir", 
+      date: "5 Mei 2025", 
+      summary: "Kubu Raya, Berkatnewstv, Kehadiran PT Win Global Solusitama (WGS) semakin hari kian mendapat tempat di...",
+      imgSrc: "/rentenir.jpg",
+      sourceUrl: "https://berkatnewstv.com/wgs-solusi-atasi-warga-desa-dari-jeratan-rentenir/" // URL sumber
+    },
+    { 
+      id: 3, 
+      title: "WGS Melayani Pembiayaan untuk Semua Umat", 
+      date: "30 April 2025", 
+      summary: "Pontianak, Berkatnewstv, PT Win Global Solusitama (WGS), sebuah perusahaan star-up berbasis syariah yang serius...",
+      imgSrc: "/semuaumat.jpg",
+      sourceUrl: "https://berkatnewstv.com/wgs-melayani-pembiayaan-untuk-semua-umat/" // URL sumber
+    },
+    { 
+      id: 2, 
+      title: "WGS Angin Segar UMKM di Tengah Ekonomi Tidak Stabil", 
+      date: "29 April 2025", 
+      summary: "Pontianak, Berkatnewstv, Wakil Ketua Pontianak City Rudi Kamtono mengapresiasi positif kehadiran PT Win Global Solusitama...",
+      imgSrc: "/angin.jpg",
+      sourceUrl: "https://berkatnewstv.com/wgs-angin-segar-umkm-di-tengah-ekonomi-tidak-stabil/" // URL sumber
+    },
+    { 
+      id: 1, 
+      title: "Pemprov Kalbar Dukung WGS Bantu Pembiayaan UMKM", 
+      date: "29 April 2025", 
+      summary: "Pontianak, Berkatnewstv, Pemprov Kalbar menyatakan dukungannya terhadap PT Win Global Solusitama (WGS) membantu pembiayaan UMKM...",
+      imgSrc: "/pemprov.jpg",
+      sourceUrl: "https://berkatnewstv.com/pemprov-kalbar-dukung-wgs-bantu-pembiayaan-umkm/" // URL sumber
+    },
 ];
-
 // Data Galeri (pertahankan jika kamu tidak ingin mengganti)
 export const galleryItems = [
-    { id: 1, src: "/slideshow/gmbr1.jpg", alt: "Acara Lapangan" },
-    { id: 2, src: "/slideshow/gmbr2.jpg", alt: "Tim WGS" },
-    { id: 3, src: "/slideshow/gmbr3.jpg", alt: "Rapat Direksi" },
-    { id: 4, src: "/slideshow/gmbr4.jpg", alt: "Produk Unggulan" },
-    { id: 5, src: "/slideshow/gmbr5.jpg", alt: "Produk Unggulan" },
-    { id: 6, src: "/slideshow/gmbr6.jpg", alt: "Produk Unggulan" },
-    { id: 7, src: "/slideshow/gmbr7.jpg", alt: "Produk Unggulan" },
-    { id: 8, src: "/slideshow/gmbr8.jpg", alt: "Produk Unggulan" },
+    { id: 1, src: "/slideshow/img01.JPG", alt: "Acara Lapangan" },
+    { id: 2, src: "/slideshow/img02.JPG", alt: "Tim WGS" },
+    { id: 3, src: "/slideshow/img03.jpeg", alt: "Rapat Direksi" },
+    { id: 4, src: "/slideshow/img04.jpeg", alt: "Produk Unggulan" },
+    { id: 5, src: "/slideshow/gmbr1.jpg", alt: "Acara Lapangan" },
+    { id: 6, src: "/slideshow/gmbr2.jpg", alt: "Tim WGS" },
+    { id: 7, src: "/slideshow/gmbr3.jpg", alt: "Rapat Direksi" },
+    { id: 8, src: "/slideshow/gmbr4.jpg", alt: "Produk Unggulan" },
+    { id: 9, src: "/slideshow/gmbr5.jpg", alt: "Produk Unggulan" },
+    { id: 10, src: "/slideshow/gmbr6.jpg", alt: "Produk Unggulan" },
+    { id: 11, src: "/slideshow/gmbr7.jpg", alt: "Produk Unggulan" },
+    { id: 12, src: "/slideshow/gmbr8.jpg", alt: "Produk Unggulan" },
 ];
